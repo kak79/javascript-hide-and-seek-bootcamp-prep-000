e3f8fe316d9c5346d88337ee82e87a91bbd36e83
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-19 21:04:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deepestChild(){
   
   var node = document.getElementById('#grand-node');
 
-     while(node.hasChildNodes){
+     while(node.childNodes){
         
         //    return node;
         //else
