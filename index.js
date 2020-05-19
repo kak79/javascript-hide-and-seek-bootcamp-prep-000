0016fb7c402d90a4a7b5675a404e3df09ba2ad19
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-19 21:00:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ function increaseRankBy(n){
 function deepestChild(){
   
   var node = document.getElementById('#grand-node');
-    
+
+    for (i=0; i<?; i++){
+    document.querySelectorAll(selector)
+  }
     
     
 }
