ea32cc6379f84ca55ea279b2f82f8579931fce9e
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-19 21:31:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function increaseRankBy(n){
 function deepestChild(){
   
   var parent = document.getElementById('#grand-node');
-  var child = parent.Children[0];
+  var child = parent.children[0];
 
      while(child ){                 //make the parent the child
         if(!child){    //make child one level deeper child
