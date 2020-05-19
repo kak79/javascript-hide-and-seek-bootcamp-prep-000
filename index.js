64a97778f0f44ec484dc6071726b366c824f204f
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-19 22:15:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+    const list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+    for (let i = 0; i < list.length; i++) {
+        list[i].innerHTML = (parseInt(list[i].innerHTML) + n);
+      }
+}
+
+function deepestChild(){
+  
+  var parent = document.getElementById('grand-node');
+  var child = parent.children[0];
+
+     while(child){                 //make the parent the child
+     parent = child
+     
+     child = parent.children[0];
+     
+     }
+     
+     return parent;
+}
