048cbcd7dfc74b3195bfef5b67bb3b3d308555b3
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-19 20:16:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ function nestedTarget(){
   return document.querySelector('#nested .target')
 }
 
+
+function 
+
+
 function increaseRankBy(n){
-  return document
+  return document.ranked-list(n)
 }
 
 
